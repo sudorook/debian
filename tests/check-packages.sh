9b10ac350111e0da362030e59ab5e26c866465fd
pseudorook <pseudorook@protonmail.com>
2018-01-20 20:57:26 -0500

    have check-packages.sh exit with error code 1 if any packages are missing

--- tests/check-packages.sh
+++ tests/check-packages.sh
@@ -14,14 +14,18 @@ pkgdir=../packages
 for file in ${pkgdir}/*; do
   echo $(basename ${file})
 
+  missing=false
   for package in $(cat ${file}); do
     if apt-cache search --names-only ^$(clean_pkgname "${package}")$ >/dev/null; then
     # if dpkg -s ^$(clean_pkgname "${package}")$ >/dev/null; then
       echo "✓ ${package}"
     else
+      missing=true
       echo "✗ ${package}"
     fi
   done
 
   echo
 done
+
+if $missing; then exit 1; fi
